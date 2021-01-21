af4cb3fcae1bcdf13e63465ea1c672d0efa8345b
achernys <andrey.chernyshov@intel.com>
2021-01-21 14:42:39 +0300

    rev1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from random import shuffle
+from queue import Queue
+from typing import List
 
 
 class DrunkerGeneralException(Exception):
@@ -6,6 +8,10 @@ class DrunkerGeneralException(Exception):
 
 
 class InvalidCardException(DrunkerGeneralException):
+    pass
+
+
+class PlayerOutOfCardsException(DrunkerGeneralException):
     pass
 
 
@@ -30,6 +36,9 @@ class Card:
             return True
         return False
 
+    def __repr__(self):
+        return self.value
+
 
 class Deck:
 
@@ -41,3 +50,71 @@ class Deck:
 
     def shuffle(self):
         shuffle(self.all_deck)
+
+
+class Player:
+
+    def __init__(self):
+        self.hand = Queue()
+
+    def move(self):
+        if not self.hand.empty():
+            return self.hand.get()
+        else:
+            raise PlayerOutOfCardsException
+
+    def take(self, cards: List[Card]):
+        for card in cards:
+            self.hand.put(card)
+
+
+class Game:
+
+    def __init__(self):
+        self.player1 = Player()
+        self.player2 = Player()
+        deck = Deck()
+        deck.shuffle()
+        self.deck = deck.all_deck
+        self.deal()
+
+    def deal(self):
+        self.player1.take(self.deck[0:len(self.deck) // 2])
+        self.player2.take(self.deck[len(self.deck) // 2:len(self.deck) + 1])
+
+    def play(self):
+        bank = []
+        step = 0
+        while True:
+            step += 1
+            try:
+                card1 = self.player1.move()
+            except PlayerOutOfCardsException:
+                print("Player 1 has lost the game")
+                break
+
+            try:
+                card2 = self.player2.move()
+            except PlayerOutOfCardsException:
+                print("Player 2 has lost the game")
+                break
+
+            bank.append(card1)
+            bank.append(card2)
+
+            if card1 > card2:
+                self.player1.take(bank)
+                bank = []
+            elif card2 > card1:
+                self.player2.take(bank)
+                bank = []
+            elif card1 == card2:
+                continue
+
+        print(f"Number of steps: {step}")
+
+
+if __name__ == "__main__":
+    game = Game()
+
+    game.play()
